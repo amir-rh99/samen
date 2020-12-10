4ca0c3d861a9680b918d4c063a1c48cb43784b26
AmiReza Hammamian <amir.astar20@gmail.com>
2020-12-10 16:40:45 +0330

    index.ts added to pages and componets & app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,45 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { LostPasswordComponent } from './pages/lost-password/lost-password.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
-import { DialogComponent } from './components/dialog/dialog.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoadingComponent } from './components/loading/loading.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 import { provideRoutes, RouterState } from '@angular/router';
-import { MobileSidebarComponent } from './components/mobile-sidebar/mobile-sidebar.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { EnrollComponent } from './pages/enroll/enroll.component';
-import { TestRunnerComponent } from './pages/test-runner/test-runner.component';
-import { ResultComponent } from './pages/result/result.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { NotificationsComponent } from './components/notifications/notifications.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import {
+  UsersComponent,
+  EditUserComponent,
+  BusinessPartnerComponent,
+  AuthComponent,
+  EnrollComponent,
+  HomeComponent,
+  LandingComponent,
+  LoginComponent,
+  LostPasswordComponent,
+  NotFoundComponent,
+  ProfileComponent,
+  RegisterComponent,
+  ResultComponent,
+  ServicesComponent,
+  TestRunnerComponent
+} from './pages'
+
+import {
+  UserInfoComponent,
+  ChangeAvatarComponent,
+  ChangeInfoComponent,
+  ChangePasswordComponent,
+  BtnLoadingComponent,
+  BreadcrumbComponent,
+  CommentsComponent,
+  DialogComponent,
+  FooterComponent,
+  LoadingComponent,
+  MobileSidebarComponent,
+  SidebarComponent,
+  NavbarComponent,
+  NotificationsComponent,
+} from './components'
 
 import {MatIconModule} from '@angular/material/icon';
 
@@ -37,26 +52,38 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
+    //pages
+    UsersComponent,
+    EditUserComponent,
+    BusinessPartnerComponent,
+    AuthComponent,
+    EnrollComponent,
+    HomeComponent,
     LandingComponent,
     LoginComponent,
-    RegisterComponent,
-    FooterComponent,
-    SidebarComponent,
-    ServicesComponent,
     LostPasswordComponent,
-    AuthComponent,
-    BreadcrumbComponent,
-    DialogComponent,
-    HomeComponent,
-    LoadingComponent,
-    MobileSidebarComponent,
-    EnrollComponent,
-    TestRunnerComponent,
+    NotFoundComponent,
+    ProfileComponent,
+    RegisterComponent,
     ResultComponent,
-    CommentsComponent,
-    NotificationsComponent
+    ServicesComponent,
+    TestRunnerComponent,
+
+    // components
+   UserInfoComponent,
+   ChangeAvatarComponent,
+   ChangeInfoComponent,
+   ChangePasswordComponent,
+   BtnLoadingComponent,
+   BreadcrumbComponent,
+   CommentsComponent,
+   DialogComponent,
+   FooterComponent,
+   LoadingComponent,
+   MobileSidebarComponent,
+   SidebarComponent,
+   NavbarComponent,
+   NotificationsComponent,
   ],
   imports: [
     BrowserModule,
